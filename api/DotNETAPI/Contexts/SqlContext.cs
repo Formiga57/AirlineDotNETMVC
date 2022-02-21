39d53d81b0884243722ca1e122f58efc822f43ef
Vinícius Formigone <formiga5557@gmail.com>
2022-02-21 00:28:05 -0300

    Adição de aeroportos e cidades padrões

--- api/DotNETAPI/Contexts/SqlContext.cs
+++ api/DotNETAPI/Contexts/SqlContext.cs
@@ -14,6 +14,32 @@ public class SqlContext : DbContext
         modelBuilder.Entity<Assento>()
             .Property(b => b.Reservado)
             .HasDefaultValue(false);
+        modelBuilder.Entity<Cidade>().HasData(new Cidade[] {
+            new Cidade {Id = 1,Nome = "São Paulo",UF="SP"},
+            new Cidade {Id = 2,Nome = "Belo Horizonte",UF="BH"},
+            new Cidade {Id = 3,Nome = "Rio de Janeiro",UF="RJ"},
+            new Cidade {Id = 4,Nome = "Navegantes",UF="SC"},
+            new Cidade {Id = 5,Nome = "Foz do Iguaçu",UF="PR"},
+            new Cidade {Id = 6,Nome = "Fortaleza",UF="CE"}
+        });
+        modelBuilder.Entity<Aeroporto>().HasData(new Aeroporto[] {
+            new Aeroporto {Id = 1,Nome = "Guarulhos",Cód = "GRU"},
+            new Aeroporto {Id = 2,Nome = "Congonhas",Cód = "CGH"},
+            new Aeroporto {Id = 3,Nome = "Santos Dumont",Cód = "SDU"},
+            new Aeroporto {Id = 4,Nome = "Confins",Cód = "CNF"},
+            new Aeroporto {Id = 5,Nome = "Navegantes",Cód = "NVT"},
+            new Aeroporto {Id = 6,Nome = "Foz do Iguaçu",Cód = "IGR"},
+            new Aeroporto {Id = 7,Nome = "Fortaleza",Cód = "FOR"}
+        });
+        modelBuilder.Entity<AeroportoCidade>().HasData(new AeroportoCidade[] {
+            new AeroportoCidade{Id=1,CidadeFk = 1,AeroportoFk = 1},
+            new AeroportoCidade{Id=2,CidadeFk = 1,AeroportoFk = 2},
+            new AeroportoCidade{Id=3,CidadeFk = 2,AeroportoFk = 4},
+            new AeroportoCidade{Id=4,CidadeFk = 3,AeroportoFk = 3},
+            new AeroportoCidade{Id=5,CidadeFk = 4,AeroportoFk = 5},
+            new AeroportoCidade{Id=6,CidadeFk = 5,AeroportoFk = 6},
+            new AeroportoCidade{Id=7,CidadeFk = 6,AeroportoFk = 7},
+        });
     }
     public DbSet<Usuário> Usuários { get; set; }
     public DbSet<UsuárioVoo> UsuáriosVoos { get; set; }
